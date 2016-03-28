303c0f993b6b4bbe99e512918d19a5eb5ad68293
komali2 <rogersjcaleb@gmail.com>
2016-03-28 16:37:59 -0700

    core: deployment-ready and tested for google cloud

--- server.js
+++ server.js
@@ -15,6 +15,7 @@ app.get('/style.css', function(req, res){
   res.sendFile(__dirname + '/app/client/style.css');
 })
 
+//deploy-ready
 app.listen(process.env.PORT || 3000, function(){
   console.log("Prompter listening on port 3000.");
 });
