89158a0b4abbd3db41e5eab7de92614774b15c09
komali2 <rogersjcaleb@gmail.com>
2016-03-28 12:13:21 -0700

    feat: basic prompt array built

--- app/client/prompter.js
+++ app/client/prompter.js
@@ -1,6 +1,14 @@
 var app = angular.module('writingPrompter', []);
 
-  app.service('promptServicer', function(){});
+  app.service('promptServicer', function(){
+    this.promptList = [];
+    var prompt1 = "Your friend was struck by lightning.";
+    var prompt2 = "Describe how fingers feel to fingers.";
+    this.promptList.push(prompt1, prompt2)
+    this.promptShow = function(){
+
+    }
+  });
 
   app.controller('promptController', function($scope, promptServicer){
 
