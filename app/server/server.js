bed3eb3248d773a9cbc5d9af5ccd4fc78644e297
komali2 <rogersjcaleb@gmail.com>
2016-03-28 12:04:34 -0700

    feat: server serves static pages to client folder

--- app/server/server.js
+++ app/server/server.js
@@ -1,12 +1,14 @@
 var express = require('express');
 var app = express();
 
+app.use(express.static(__dirname + '/../client'));
+
 app.get('/', function(req, res){
-  res.send('Hello World!');
+  res.sendFile(__dirname + '/../client/index.html');
 });
 
 app.get('/prompter.js', function(req, res){
-  res.sendFile(__dirname + '/../prompter.js');
+  res.sendFile(__dirname + '/../client/prompter.js');
 })
 
 app.listen(3000, function(){
