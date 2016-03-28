db19160fb32b0e328f2c759bc282657b88064b57
komali2 <rogersjcaleb@gmail.com>
2016-03-28 14:48:16 -0700

    feat: added more prompts, cleaned up index.html, added comments

--- app/client/prompter.js
+++ app/client/prompter.js
@@ -5,21 +5,27 @@ function randArrayElement(array) {
   return array[Math.floor(Math.random() * array.length)];
 }
 
-
+  //service function for holding logic
   app.service('promptServicer', function(){
     this.promptList = [];
     var prompt1 = "Your friend was struck by lightning.";
     var prompt2 = "Describe how fingers feel to fingers.";
-    this.promptList.push(prompt1, prompt2)
+    var prompt3 = "Nothing is quite as satisfying as crushing small bones beneath one's feet.";
+    var prompt4 = "Help me bury this body!";
+    var prompt5 = "Where did you stash the DNA?";
+    this.promptList.push(prompt1, prompt2, prompt3, prompt4, prompt5);
 
     this.promptShow = function(){
       return randArrayElement(this.promptList);
     }
   });
 
+  //controller for all things dealing with the prompt area in the view, index.html
   app.controller('promptController', function($scope, promptServicer){
+    //happens on click in the index.html
     $scope.promptShow = function(){
-      console.log('hello');
+      //sets the {{prompt}} area in index.html to a random element in the
+      //prompts array
       $scope.prompt = promptServicer.promptShow();
     }
   });
