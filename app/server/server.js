582ec3e3abbc46a788be467813da013cc0573d43
komali2 <rogersjcaleb@gmail.com>
2016-03-28 12:02:53 -0700

    core: file structure established

--- app/server/server.js
+++ app/server/server.js
@@ -5,6 +5,10 @@ app.get('/', function(req, res){
   res.send('Hello World!');
 });
 
+app.get('/prompter.js', function(req, res){
+  res.sendFile(__dirname + '/../prompter.js');
+})
+
 app.listen(3000, function(){
   console.log("Prompter listening on port 3000.");
 });
