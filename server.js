bb5e5cefe2c087e5a1710b7df385f2f2f7ab5df4
komali2 <rogersjcaleb@gmail.com>
2016-03-29 10:42:01 -0700

    core: structure of reddit api call build out

--- server.js
+++ server.js
@@ -2,6 +2,28 @@ var express = require('express');
 var app = express();
 var http = require('http');
 
+var options = {
+  host: 'www.reddit.com',
+  path: '/r/writingprompts/new.json?sort=new'
+};
+
+var redReq = http.get(options, function(res){
+  console.log('STATUS: ' + res.statusCode);
+  console.log('HEADERS: ' + JSON.stringify(res.headers));
+
+  var bodyChunks = [];
+  res.on('data', function(chunk){
+    bodyChunks.push(chunk);
+  }).on('end', function(){
+    var body = Buffer.concat(bodyChunks);
+    console.log('BODY: ' + body);
+  })
+});
+
+redReq.on('error', function(e){
+  console.log('ERROR: ' + e.message);
+});
+
 app.use(express.static(__dirname + '/app/client'));
 
 app.get('/', function(req, res){
