1eb24292ecf1d56ab2a26f03fe952e8d1b816a53
komali2 <rogersjcaleb@gmail.com>
2016-03-29 10:57:02 -0700

    core: made the promptsarray available to the client through a get request

--- server.js
+++ server.js
@@ -7,30 +7,33 @@ var options = {
   path: '/r/writingprompts/new.json?sort=new'
 };
 var promptsArray = [];
-var redReq = http.get(options, function(res){
-  //console.log('STATUS: ' + res.statusCode);
-  //console.log('HEADERS: ' + JSON.stringify(res.headers));
 
+/*----------------------
+REDDIT API
+------------------------*/
+
+//will send a GET request to the reddit api
+var redReq = http.get(options, function(res){
+  //filled with chunks from the response
   var bodyChunks = [];
   res.on('data', function(chunk){
     bodyChunks.push(chunk);
   }).on('end', function(){
     var body = Buffer.concat(bodyChunks);
     body = JSON.parse(body);
-    //console.log('BODY data keys: ' + Object.keys(body.data.children));
-    //console.log('BODY data text:' + JSON.stringify(body.data.children[0].data.title));
-    for(var i = 0; i < body.data.children.length; i++){
+      //adds the titles from the /new prompts into an array
       promptsArray.push(body.data.children[i].data.title);
-
     }
-    console.log(promptsArray);
   });
 });
-
-
+//reddit request error handling
 redReq.on('error', function(e){
   console.log('ERROR: ' + e.message);
 });
+
+/*----------------------
+ROUTING
+------------------------*/
 
 app.use(express.static(__dirname + '/app/client'));
 
@@ -44,9 +47,17 @@ app.get('/prompter.js', function(req, res){
 
 app.get('/style.css', function(req, res){
   res.sendFile(__dirname + '/app/client/style.css');
-})
+});
 
-//deploy-ready
+app.get('/reddit', function(req, res){
+  res.send(promptsArray);
+});
+
+/*----------------------
+SERVER INIT
+------------------------*/
+
+
 app.listen(process.env.PORT || 3000, function(){
   console.log("Prompter listening on port 3000.");
 });
