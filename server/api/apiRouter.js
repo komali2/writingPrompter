19c3d511078b82bd109b8d07d7b25b5cc912f211
komali2 <rogersjcaleb@gmail.com>
2016-05-20 10:43:00 -0700

    bug: removed period from user route

--- server/api/apiRouter.js
+++ server/api/apiRouter.js
@@ -4,6 +4,6 @@ var userController = require('./userController.js');
 module.exports = function(router){
   router.get('/reddit', redditController);
   router.get('/user', userController.get);
-  router.post('./user', userController.post);
+  router.post('/user', userController.post);
   router.delete('/user', userController.delete);
 };
