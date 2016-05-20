9a184315a23248ee554a23b3feef301850d18e9f
komali2 <rogersjcaleb@gmail.com>
2016-05-20 11:48:29 -0700

    core: built out structure for localFactory

---
+++ app/client/factories/localFactory.js
@@ -0,0 +1,15 @@
+angular.module('writingPrompter', [])
+  .factory('localFactory', function(helperFactory){
+    var exports = {};
+    var promptList = [];
+    var prompt1 = "Your friend was struck by lightning.";
+    var prompt2 = "Describe how fingers feel to fingers.";
+    var prompt3 = "Nothing is quite as satisfying as crushing small bones beneath one's feet.";
+    var prompt4 = "Help me bury this body!";
+    var prompt5 = "Where did you stash the DNA?";
+    promptList.push(prompt1, prompt2, prompt3, prompt4, prompt5);
+
+    exports.getPrompts = function(){
+      return helperFactory.randArrayElement(promptList);
+    };
+  });
