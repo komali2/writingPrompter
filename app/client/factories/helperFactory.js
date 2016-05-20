84a804aeeef2dc21cb681841545d7b2e1cc5462e
komali2 <rogersjcaleb@gmail.com>
2016-05-20 11:51:11 -0700

    core: added randarray function to helper

--- app/client/factories/helperFactory.js
+++ app/client/factories/helperFactory.js
@@ -1,4 +1,8 @@
 angular.module('writingPrompter', [])
   .factory('helperFactory', function(){
-    
+    var exports = {};
+
+    exports.randArrayElement = function(array){
+      return array[Math.floor(Math.random() * array.length)];
+    };
   });
