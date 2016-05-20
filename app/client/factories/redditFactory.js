b24ce2f9972503e0eead999a6e9a7be40c6b1bc9
komali2 <rogersjcaleb@gmail.com>
2016-05-20 12:27:19 -0700

    core: added reddit factory logic

--- app/client/factories/redditFactory.js
+++ app/client/factories/redditFactory.js
@@ -1,7 +1,18 @@
 angular.module('writingPrompter')
   .factory('redditFactory', function($http){
     var exports = {};
-
+    exports.getRedditPrompts = function(cb){
+      $http({
+        method: 'GET',
+        url: '/external/reddit'
+      }).then(function success(res){
+        cb(res);
+      }, function failure(res){
+        console.log('error in getRedditPrompts factory', res);
+        cb(res);
+      });
+    };
+    
     return exports;
 
   });
