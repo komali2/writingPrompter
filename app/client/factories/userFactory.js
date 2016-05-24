dd1aae6f04c786072ed27351eae2384a491209a1
komali2 <rogersjcaleb@gmail.com>
2016-05-24 15:03:17 -0700

    core: built out getUser functionalty

--- app/client/factories/userFactory.js
+++ app/client/factories/userFactory.js
@@ -1,9 +1,19 @@
 angular.module('writingPrompter', [])
-  .factory('userFactory', function(){
+  .factory('userFactory', function($http){
     var exports = {};
 
-    exports.getPrompt = function(){};
-    exports.submitPrompt = function(){};
+    exports.getPrompt = function(cb){
+      $http({
+        method: 'GET',
+        url: '/external/user'
+      }).then(function success(res){
+        cb(res);
+      }, function failure(res){
+        console.log('error in getUserPrompts factory', res);
+        cb(res);
+      });
+    };
+    exports.submitPrompt = function(data){};
     exports.deletePrompts = function(){};
 
     return exports;
